4ee4a0d6b949115b69d5745cbf800180c03d8b9c
calestini <calestini27@gmail.com>
2018-09-24 20:36:35 -0400

    removed duplicates

--- baseball/statcast.py
+++ baseball/statcast.py
@@ -22,13 +22,17 @@ class Statcast(object):
 			'min_abs':'0','type':'details'
 		}
 
-		self.season = {'start_dt': datetime.date(2018,3,29), 'end_dt': datetime.date(2018,9,22)}
+		#2015: April 5 - Oct 4
+		#2016: April 3 - Oct 2
+		#2017: April 2 - Oct 3
+
+		self.season = {'start_dt': datetime.date(2015,4,5), 'end_dt': datetime.date(2015,10,4)}
 
 	def get_data(self, start_dt, end_dt):
 
 		r = requests.get(self.url.format(start_dt, end_dt), timeout=None)
 		print (r.status_code)
-		df = pd.read_csv(io.StringIO(r.content.decode('utf-8')), engine='python')
+		df = pd.read_csv(io.StringIO(r.content.decode('utf-8')), engine='c')
 		return df
 
 	def get_season_pitches(self):
@@ -37,14 +41,14 @@ class Statcast(object):
 		start_dt = self.season['start_dt']
 		end_dt = self.season['end_dt']
 		results = pd.DataFrame()
-		for loop, i in enumerate(range((end_dt - start_dt).days)):
+		for loop, i in enumerate(range(0,(end_dt - start_dt).days, 2)):
 			temp_start = start_dt + datetime.timedelta(days=i)
 			temp_end = temp_start + datetime.timedelta(days=1)
 			#print (temp_start, temp_end)
 			pitches = self.get_data(temp_start, temp_end)
 			results = pd.concat([results, pitches], axis = 0)
-			print (loop, len(results))
-		results.to_csv('pitches_2018.csv', index=False)
+			print (loop,temp_start, temp_end, len(results))
+		results.to_csv('pitches_2015.csv', index=False)
 
 if __name__ == '__main__':
 	pitches = Statcast()
